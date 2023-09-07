449e8f14e09eaa279a8872d54a960e808e348d05
ant1po1e <symamaxim@gmail.com>
2023-09-07 15:43:33 +0700

    added contact and footer

--- dist/js/script.js
+++ dist/js/script.js
@@ -24,30 +24,30 @@ window.addEventListener("click", function (e) {
    }
 });
 
-// const scriptURL =
-//    "https://script.google.com/macros/s/AKfycbx0eCzzKaKtZ2gIwprnJ-5xtkpoM0kqcLv5uoVVUVZZMgIsJ6Fn6YHJ5OzUIOcpIcJV/exec";
-// const form = document.forms["Ant1po1e-contact-form"];
-// const success = document.querySelector(".success");
-// const formAlert = document.querySelector(".alert");
+const scriptURL =
+   "https://script.google.com/macros/s/AKfycbx0eCzzKaKtZ2gIwprnJ-5xtkpoM0kqcLv5uoVVUVZZMgIsJ6Fn6YHJ5OzUIOcpIcJV/exec";
+const form = document.forms["Ant1po1e-contact-form"];
+const success = document.querySelector(".success");
+const formAlert = document.querySelector(".alert");
 
-// form.addEventListener("submit", (e) => {
-//    e.preventDefault();
-//    formAlert.classList.toggle("hidden");
-//    formAlert.classList.toggle("flex");
-//    success.classList.toggle("hidden");
-//    fetch(scriptURL, {
-//       method: "POST",
-//       body: new FormData(form),
-//    })
-//       .then((response) => {
-//          formAlert.classList.toggle("hidden");
-//          formAlert.classList.toggle("flex");
-//          success.classList.add("hidden");
-//          form.reset();
-//          console.log("Success!", response);
-//       })
-//       .catch((error) => console.error("Error!", error.message));
-// });
+form.addEventListener("submit", (e) => {
+   e.preventDefault();
+   formAlert.classList.toggle("hidden");
+   formAlert.classList.toggle("flex");
+   success.classList.toggle("hidden");
+   fetch(scriptURL, {
+      method: "POST",
+      body: new FormData(form),
+   })
+      .then((response) => {
+         formAlert.classList.toggle("hidden");
+         formAlert.classList.toggle("flex");
+         success.classList.add("hidden");
+         form.reset();
+         console.log("Success!", response);
+      })
+      .catch((error) => console.error("Error!", error.message));
+});
 
 const back2Top = document.querySelector("#back2top");
 
